7a257e55ecb7f94946eb09de9a4cbcf9b0c982a7
EngiBeerHub <engibeer0422@gmail.com>
2026-02-09 21:57:54 +0900

    installed uniwind and heroui native
    依存エラーは以下で解決
    - react-domをexpo install
    - reactとreact-native-gesture-handlerのバージョンを固定
    - uniwindは最新バージョンを再インストール

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import {StatusBar} from "expo-status-bar";
+import {Text, View} from "react-native";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import {Button, HeroUINativeConfig, HeroUINativeProvider} from "heroui-native";
+
+const config: HeroUINativeConfig = {
+    devInfo: {
+        // Disable styling principles information message
+        stylingPrinciples: false,
+    },
+}
 
 export default function App() {
-  return (
-    <View className="flex-1 bg-white dark:bg-black items-center justify-center px-8">
-      {/* Heading */}
-      <Text className="text-4xl font-extrabold text-gray-800 dark:text-white mb-3 tracking-tight">
-        🚀 Welcome
-      </Text>
+    return (
+        <GestureHandlerRootView>
+            <HeroUINativeProvider config={config}>
+                <View className="flex-1 bg-white dark:bg-black items-center justify-center px-8">
+                    {/* Heading */}
+                    <Text className="text-4xl font-extrabold text-gray-800 dark:text-white mb-3 tracking-tight">
+                        🚀 Welcome
+                    </Text>
 
-      {/* Subheading */}
-      <Text className="text-xl dark:text-white text-gray-700 mb-8 text-center leading-relaxed">
-        Build beautiful apps with{" "}
-        <Text className="text-blue-500 font-semibold">
-          Expo (Router) + Uniwind 🔥
-        </Text>
-      </Text>
+                    {/* Subheading */}
+                    <Text className="text-xl dark:text-white text-gray-700 mb-8 text-center leading-relaxed">
+                        Build beautiful apps with{" "}
+                        <Text className="text-blue-500 font-semibold">
+                            Expo (Router) + Uniwind 🔥
+                        </Text>
+                    </Text>
 
-      {/* Instruction text */}
-      <Text className="text-base text-gray-600 dark:text-white text-center max-w-sm">
-        Start customizing your app by editing{" "}
-        <Text className="font-semibold text-gray-800 dark:text-white">
-          app/index.tsx
-        </Text>
-      </Text>
+                    {/* Instruction text */}
+                    <Text className="text-base text-gray-600 dark:text-white text-center max-w-sm">
+                        Start customizing your app by editing{" "}
+                        <Text className="font-semibold text-gray-800 dark:text-white">
+                            app/index.tsx
+                        </Text>
+                    </Text>
 
-      <StatusBar style="dark" />
-    </View>
-  );
+                    <Button onPress={() => console.log('Pressed!')}>Get Started</Button>
+
+                    <StatusBar style="dark"/>
+                </View>
+            </HeroUINativeProvider>
+        </GestureHandlerRootView>
+    );
 }
